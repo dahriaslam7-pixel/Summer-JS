806bbb289e4007b041d3ec7146cf923f03cd00ba
Aslam Dahri <225027466+dahriaslam7-pixel@users.noreply.github.com>
2026-05-31 12:14:47 +0000

    This my practice on data type

--- data.js
+++ data.js
@@ -1,1 +1,23 @@
-// 
+// Data types are basically used to identify values which is stored or assign by any variiables.
+// In javaScript there are String,Integer,character,Boolean for true and false values,float,
+// ==>Integer: Used for numbers
+// ==>Charagter is any single alphabatic character single quote is used note whenever we write any number in single quote this will be also considered in character not in number.
+// ==>String is group of character.Double quotes are used to represent a string.
+// ==> boolean or true/false value 1 is used for true and 0 is for false.
+// ==?This means whenever 1 accurs the result is true and 0 accures the result is false as well.
+
+// ==> AS WE KNOW THAT JS IS DYNAMICALLY TYPED LANGUAGE SO WE NO NEED TO WRITE DATA TYPE WITH ANY VARIABLE JUST INITIALIZE AND ASSIGN VALUE IN VARIABLE COMPUTER WI;LL utomatically understand and recognize data type.we can check data type by function typeof.
+
+
+// let a=3;
+// console.log(typeof(a));
+
+// let name ="Aslam";
+// console.log(typeof(name));
+
+// let islighton=true;
+// console.log(typeof(islighton));
+
+// let islighton=false;
+// console.log(typeof(islighton));
+
