228efd92854b7c7d1fa75410ca137b9b5468cfab
Aslam Dahri <225027466+dahriaslam7-pixel@users.noreply.github.com>
2026-06-01 06:42:53 +0000

    This is my third practice on data type where i try to learn some basic funtions used in data types

--- day3.js
+++ day3.js
@@ -1,1 +1,51 @@
+// ==> As we already know data types in js here we will perform some basic funtions with data type to make changes,know data tpye, no of characters in any string and how one data type is changed to another data type etc.
+
+// a="Hello ";
+// b="Aslam";
+// console.log(a+" "+b);
+
+
+
+// console.log(typeof a);
+// let num=3;
+// console.log(typeof num);
+
+
+// const pi=3.14;
+// console.log(typeof pi);
+
+// let isLighton=true;
+// console.log(Boolean(1));
+
+// let isLLightoff=false;
+// console.log(Boolean(isLLightoff));
+
+// Here Boolean is a function which is used to convert from false to true and true to false and aslo can be same as given.
+// ==>When we put any variable inside boolean function then resut will  be same as variable initialized.
+// We can put 0/1 inside boolean fuction for false and true respectively as well.
+
+// let isRunning=false;
+// console.log(Boolean(isRunning));
+
+// let isRunning=false;
+// console.log(Boolean(0));
+
+// let isCrying=true;
+// console.log(Boolean(0)); //false becase boolean value is 0.
+
+// let isCrying=false;
+// console.log(Boolean(1));
+
+// let name="Aslam";
+// console.log(name. length)
+
+
+// let name="Akram";
+// console.log(name.toUpperCase());
+
+// let fullName="Aslam Dahri";
+// console.log(fullName.toLocaleUpperCase());
+
+// let fullName="Aslam Dahri";
+// console.log(fullName.toLocaleLowerCase());
 
