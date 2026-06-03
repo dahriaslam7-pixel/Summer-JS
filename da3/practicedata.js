2862ba4cb28da73c91fa60578e10958c82779b30
Aslam Dahri <225027466+dahriaslam7-pixel@users.noreply.github.com>
2026-06-03 16:21:25 +0000

    Practice on all topics and arranging in order

---
+++ da3/practicedata.js
@@ -0,0 +1,82 @@
+// ==> As we already know data types in js here we will perform some basic funtions with data type to make changes,know data tpye, no of characters in any string and how one data type is changed to another data type etc.
+
+// a="Hello ";
+// b="Aslam";
+// console.log(a+" "+b);
+
+
+
+// console.log(typeof a);
+// let num=3;
+// console.log(typeof num);
+
+
+// const pi=3.14;
+// console.log(typeof pi);
+
+// let isLighton=true;
+// console.log(Boolean(1));
+
+// let isLLightoff=false;
+// console.log(Boolean(isLLightoff));
+
+// Here Boolean is a function which is used to convert from false to true and true to false and aslo can be same as given.
+// ==>When we put any variable inside boolean function then resut will  be same as variable initialized.
+// We can put 0/1 inside boolean fuction for false and true respectively as well.
+
+// let isRunning=false;
+// console.log(Boolean(isRunning));
+
+// let isRunning=false;
+// console.log(Boolean(0));
+
+// let isCrying=true;
+// console.log(Boolean(0)); //false becase boolean value is 0.
+
+// let isCrying=false;
+// console.log(Boolean(1));
+
+// let name="Aslam";
+// console.log(name. length)
+
+
+// let name="Akram";
+// console.log(name.toUpperCase());
+
+// let fullName="Aslam Dahri";
+// console.log(fullName.toLocaleUpperCase());
+
+// let fullName="Aslam Dahri";
+// console.log(fullName.toLocaleLowerCase());
+
+// let age;
+// console.log(age);
+
+// let age=null;
+// console.log(age);
+// let age=null;
+// let num=36543439909;
+// console.log(typeof num);
+// console.log(typeof age);
+
+//==> In string we can not reverse directly so first we seperate string by using split methode then reverse given splited variable then we join reversed as a final result.
+// let name="Aslam";
+// let reversed=name.split("").reverse().join("");
+// console.log(reversed);
+
+// let name="Aslam";
+// name1=name.split("");
+// console.log(name1);
+// name2=name1.reverse();
+// console.log(name2);
+// name3=name2.join("");
+// console.log(name3);
+
+// let fullName="Aslam Dahri";
+// splitedname=fullName.split("");
+// console.log(splitedname);
+// reversedname=splitedname.reverse();
+// console.log(reversedname);
+// joinreversed_in_order=reversedname.join("");
+// console.log(joinreversed_in_order);
+
